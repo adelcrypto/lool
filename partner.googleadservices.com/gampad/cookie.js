33ad12c9ebbc8bd7096300ad32e38535ad96cd19
adelcrypto <106496947+adelcrypto@users.noreply.github.com>
2022-10-19 20:41:23 +0300

    Add files via upload

---
+++ partner.googleadservices.com/gampad/cookie.js
@@ -0,0 +1,15 @@
+_gfp_s_({
+    "_cookies_": [{
+        "_value_": "ID=ae751cfc388f11d0-22f47fb013d60037:T=1666200601:RT=1666200601:S=ALNI_MYUomrYB0FeMUmO5dx6xdJL5TMcTA",
+        "_expires_": 1699896601,
+        "_path_": "/",
+        "_domain_": "herokuapp.com",
+        "_version_": 1
+    }, {
+        "_value_": "UID=00000b137bbf7af1:T=1666200601:RT=1666200601:S=ALNI_MaKlzg8GxWRuJf-1L_lsvaEmiRH4g",
+        "_expires_": 1699896601,
+        "_path_": "/",
+        "_domain_": "herokuapp.com",
+        "_version_": 2
+    }]
+});
