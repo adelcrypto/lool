3a9cc858dd69fb00c878cc2e755b86a32140eaec
adelcrypto <106496947+adelcrypto@users.noreply.github.com>
2022-10-19 20:46:04 +0300

    Delete adservice.google.com/adsid directory

--- adservice.google.com/adsid/integrator.js
+++
@@ -1,7 +0,0 @@
-processGoogleToken({
-    "newToken": "ChEI8MC-mgYQ87OC9o3LisexARI9AL7OANrESjCC25xksJNCgLSHbstsngiK7usaXEWKZfZRZbOO-HgrnaF9VJHgOad332gsQG37hlwbco6f2w",
-    "validLifetimeSecs": 300,
-    "freshLifetimeSecs": 300,
-    "1p_jar": "2022-10-19-16",
-    "pucrd": ""
-});
